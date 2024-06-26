eea78a796885637125cb97ce38cac8479b0d913d
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2024-06-26 17:57:19 +0300

    solution.js

--- 01-Fundamentals-Part-1/Coding Challenge #1/solution.js
+++ 01-Fundamentals-Part-1/Coding Challenge #1/solution.js
@@ -1,29 +1,31 @@
-function calculateBMI(mass, height) {
-  return mass / (height ** 2);
-}
-
 // Data 1
+
 let markMass1 = 78;
-let markHeight1 = 1.69;
+let markHeight1= 1.69;
+
+let MarkBMI1 = markMass1 / markHeight1**2;
+
 let johnMass1 = 92;
-let johnHeight1 = 1.95;
+let johnHeight1= 1.95;
 
-let markBMI1 = calculateBMI(markMass1, markHeight1);
-let johnBMI1 = calculateBMI(johnMass1, johnHeight1);
+let johnBMI1 = johnMass1 / johnHeight1**2;
 
-let markHigherBMI1 = markBMI1 > johnBMI1;
+let markHigherBMI1 = MarkBMI1 > johnBMI1;
 
-console.log("Data 1 - Mark has a higher BMI:", markHigherBMI1);
+console.log(MarkBMI1,johnBMI1,markHigherBMI1);
 
-// Data 2
+// Data 2 
+
 let markMass2 = 95;
-let markHeight2 = 1.88;
+let markHeight2= 1.88;
+
+let MarkBMI2 = markMass2 / markHeight2**2;
+
 let johnMass2 = 85;
-let johnHeight2 = 1.76;
+let johnHeight2= 1.76;
 
-let markBMI2 = calculateBMI(markMass2, markHeight2);
-let johnBMI2 = calculateBMI(johnMass2, johnHeight2);
+let johnBMI2 = johnMass2 / johnHeight2**2;
 
-let markHigherBMI2 = markBMI2 > johnBMI2;
+let markHigherBMI2 = MarkBMI2 > johnBMI2;
 
-console.log("Data 2 - Mark has a higher BMI:", markHigherBMI2);
+console.log(MarkBMI2,johnBMI2,markHigherBMI2);
